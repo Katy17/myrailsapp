7a34cd3050eab6d025ab4fef1aafeeeba67134bf
Katy Mesgarian <k.mesgarian@gmail.com>
2017-10-30 10:17:05 +0000

    4.4 orders model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019151313) do
+ActiveRecord::Schema.define(version: 20171026133336) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float "total"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
