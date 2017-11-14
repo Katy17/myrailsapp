cf4e9eaead203f309824e814772aae70ee2fd429
Katy Mesgarian <k.mesgarian@gmail.com>
2017-11-14 15:29:29 +0000

    5.1 Routes-root updated

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'static_pages#landing_page'
+  #rerooted from 'static_pages#landing_page'
+  root 'products#index'
 
 end
