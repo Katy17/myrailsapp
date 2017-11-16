f7a65fb091223892a7c8c84898fa6d096f1033de
Katy Mesgarian <k.mesgarian@gmail.com>
2017-11-16 21:30:57 +0000

    5.2 Forms-search form feature updated for production env

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    if Rails.env.production?
+      Product.where("name ilike ?", "%#{search_term}")
+    else
+      Product.where("name LIKE ?", "%#{search_term}%")
   end
 end
