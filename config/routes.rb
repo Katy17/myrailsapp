5457a3a9c367776cc7bb07a22c5cd8d23fae3dae
Katy Mesgarian <k.mesgarian@gmail.com>
2017-11-16 14:10:58 +0000

    5.2 Forms-search form feature added

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #rerooted from 'static_pages#landing_page'
-  root 'products#index'
+  root 'static_pages#landing_page'
 
 end
