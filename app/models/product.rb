203cac35e3cd92b39160054eb5dbee81399f4bdb
Katy Mesgarian <k.mesgarian@gmail.com>
2017-11-30 12:33:48 +0000

    5.7 Comment Model craeted

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :comments
   def self.search(search_term)
     if Rails.env.production?
       Product.where("name ilike ?", "%#{search_term}%")
@@ -6,6 +7,15 @@ class Product < ApplicationRecord
       Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+  
 end
 
 
