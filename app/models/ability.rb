3c0828a34117f5ef3a48aea4ee65737fa3be0ce1
Katy Mesgarian <k.mesgarian@gmail.com>
2017-12-06 14:00:06 +0000

    5.10 admin-role bug-fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user (not logged in)
-    can :read, :all . # permission for every user, even if not logged in
-    if user.present?  # additional permissions for logged in users (they can manage their posts)
-      can :manage, Comment, user_id: user.id 
-      if user.admin?  # additional permissions for administrators
-        can :manage, :all
-      end
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
     end
   end
   
