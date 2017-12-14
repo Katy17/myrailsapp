f87b8cd3917737f8676d37bc50799da4a08dc914
Katy Mesgarian <k.mesgarian@gmail.com>
2017-12-14 11:23:42 +0000

    6.3 functional tests added

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,25 +1,25 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe Product do 
+  describe Product do 
 
-  context "when the product has comments" do
-    let(:product) { Product.create!(name: "race bike") }
-    let(:user) { User.create!(first_name: "John", last_name: "Smith", email: "j.smith@example.com", password: "xyzxyz") }
+    before do
+      @user = FactoryGirl.create(:user)
+      @product = FactoryGirl.create(:product)
+
+      @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+      @product.comments.create!(rating: 3, user: @user, body: "Ok bike!")
+      @product.comments.create!(rating: 5, user: @user, body: "Great bike!")
+    end
+
+    it "returns the average rating of all comments" do 
+      expect(@product.average_rating).to eq(3)
+    end
+
+    it "is not valid without a product name" do
+      expect(Product.new(description: "Nice bike")).not_to be_valid
+    end
+
   end
-
-  it "returns the average rating of all comments" do 
-    product.comments.create!(rating: 1, user: user, body: "Awful bike!")
-    product.comments.create!(rating: 3, user: user, body: "Ok bike!")
-    product.comments.create!(rating: 5, user: user, body: "Great bike!")
-    expect(product.average_rating).to eq(3)
-  end
-
-
-  it "is not valid without a product name" do
-    expect(Product.new(description: "Nice bike")).not_to be_valid
-  end
-
-end
 
 
 
