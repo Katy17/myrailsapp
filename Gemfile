89ec17b07574878d87fff79e73496f9ce5231dd2
Katy Mesgarian <k.mesgarian@gmail.com>
2018-01-08 12:18:16 +0000

    6.11 Dalli gem added

--- Gemfile
+++ Gemfile
@@ -73,6 +73,7 @@ gem 'will_paginate', '~> 3.1.5'
 gem 'bootstrap-will_paginate', '1.0.0'
 gem 'rails-controller-testing'
 gem 'stripe'
+gem 'dalli'
 
 
 
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @orders = Order.includes(:product).all
   end
 
   def show
